8ccd94bde78dbc355321265d2dc88de04a14b605
Tomohiro Ikai <ikai.tomohiro@gmail.com>
2019-02-18 12:17:41 +0000

    fix multiprocessing issue in windows environment

--- src/main.py
+++ src/main.py
@@ -10,20 +10,24 @@ from trainer import Trainer
 torch.manual_seed(args.seed)
 checkpoint = utility.checkpoint(args)
 
-if args.data_test == 'video':
-    from videotester import VideoTester
-    model = model.Model(args, checkpoint)
-    t = VideoTester(args, model, checkpoint)
-    t.test()
-else:
-    if checkpoint.ok:
-        loader = data.Data(args)
+def main():
+    global model
+    if args.data_test == 'video':
+        from videotester import VideoTester
         model = model.Model(args, checkpoint)
-        loss = loss.Loss(args, checkpoint) if not args.test_only else None
-        t = Trainer(args, loader, model, loss, checkpoint)
-        while not t.terminate():
-            t.train()
-            t.test()
+        t = VideoTester(args, model, checkpoint)
+        t.test()
+    else:
+        if checkpoint.ok:
+            loader = data.Data(args)
+            model = model.Model(args, checkpoint)
+            loss = loss.Loss(args, checkpoint) if not args.test_only else None
+            t = Trainer(args, loader, model, loss, checkpoint)
+            while not t.terminate():
+                t.train()
+                t.test()
 
-        checkpoint.done()
+            checkpoint.done()
 
+if __name__ == '__main__':
+    main()
