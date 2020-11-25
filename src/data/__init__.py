5a38ac31ac91d52f1642459f039556582c8f8f38
authierj <authierj@student.ethz.ch>
2020-11-25 18:01:18 +0100

    ...

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -49,10 +49,12 @@ class Data:
             print(len(testset))
             print(len(testset[0]))
             print(len(testset[0][0]))
+            print(len(testset[0][0][0]))
+            """
             for i in testset:
                 print(len(i))
                 print("IN LOOP")
-
+            """
             self.loader_test.append(
                 dataloader.DataLoader(
                     testset,
